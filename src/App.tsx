bddbac308e8a161b4e9e50227099ba54d34ee380
Shadowforg <shdowforged@gmail.com>
2025-06-07 01:49:43 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -229,7 +229,7 @@ function App() {
             />
             
             {/* Fallback redirect */}
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<Navigate to="/\" replace />} />
           </Routes>
         </DiaryProvider>
       </AuthProvider>
